794a5ad49c46f9e0d019de8cd67bdfaa3175abce
munncoding <cjfgus983@gmail.com>
2025-04-18 17:25:41 +0900

    최초 프로젝트 생성

---
+++ dreamTravelApp/src/main/java/HyeonRi/dreamTravelApp/DreamTravelAppApplication.java
@@ -0,0 +1,13 @@
+package HyeonRi.dreamTravelApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamTravelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamTravelAppApplication.class, args);
+	}
+
+}
---
+++ dreamTravelApp/src/test/java/HyeonRi/dreamTravelApp/DreamTravelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package HyeonRi.dreamTravelApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamTravelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
