855983e3e496b9a1756156977f30dc112824ada7
munncoding <cjfgus983@gmail.com>
2025-05-07 15:38:04 +0900

    [backend] fix: JPA -> mybatis

--- TripDraw/src/main/java/HyeonRi/TripDrawApp/service/UserService.java
+++ TripDraw/src/main/java/HyeonRi/TripDrawApp/service/UserService.java
@@ -1,4 +1,17 @@
 package HyeonRi.TripDrawApp.service;
 
+import HyeonRi.TripDrawApp.dto.UserRegisterRequest;
+import HyeonRi.TripDrawApp.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserMapper userMapper;
+
+    public void register(UserRegisterRequest request) {
+        userMapper.insertUser(request);
+    }
 }
