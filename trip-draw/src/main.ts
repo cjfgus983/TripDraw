697f574ee141256f13eeac00dfa9acce1f54d063
munncoding <cjfgus983@gmail.com>
2025-05-09 11:39:42 +0900

    [frontend] fix: 페이지 라우터 연결, 메인페이지 수정

--- trip-draw/src/main.ts
+++ trip-draw/src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue';
 import router from './router';
 import './assets/main.css';
 
+// CSS
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/autoplay'
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
---
+++ trip-draw/src/shims-swiper.d.ts
@@ -0,0 +1,6 @@
+// src/shims-swiper.d.ts
+declare module 'swiper/css';
+declare module 'swiper/css/pagination';
+declare module 'swiper/css/autoplay';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/scrollbar';
