b8e1cd43b88ab38496e1650c8d7274a27528f5df
yurichoi0510 <chldbfl159@naver.com>
2025-05-16 17:23:27 +0900

    [backend] fix : secuity options, image upload 관련 수정

--- TripDraw/src/main/java/HyeonRi/TripDrawApp/config/WebConfig.java
+++ TripDraw/src/main/java/HyeonRi/TripDrawApp/config/WebConfig.java
@@ -1,7 +1,9 @@
 package HyeonRi.TripDrawApp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,7 +13,19 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Vue가 호출하는 경로
                 .allowedOrigins("http://localhost:5173") // Vue dev 서버
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+    
+    @Value("${spring.servlet.multipart.location}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	// /uploads/** 요청 → 실제 file.upload-dir 디렉터리의 파일을 반환
+        registry
+          .addResourceHandler("/uploads/**")
+          .addResourceLocations("file:" + uploadDir + "/");
     }
 }
