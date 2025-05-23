edaa3d8854e37e397e333e350d947d65f420cfd0
yurichoi0510 <chldbfl159@naver.com>
2025-05-23 13:31:00 +0900

    [frontend] places pinia 예시 변경

--- trip-draw/src/stores/places.ts
+++ trip-draw/src/stores/places.ts
@@ -12,7 +12,7 @@ export interface Place {
 export const usePlacesStore = defineStore('places', {
   state: () => ({
     // 초기 중심 위치
-    center: { name: '후지산, 일본', latitude: 35.36, longitude: 138.728 },
+    center: { name: '에펠탑, 파리', latitude: 48.8583, longitude: 2.2945 },
     // 버튼별 검색 결과
     nearby: {
       cafe: [] as Place[],
