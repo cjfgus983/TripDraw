46cb0817ea3a3598a282364ed77031eddf402e93
munncoding <cjfgus983@gmail.com>
2025-05-26 13:13:01 +0900

    Update vite.config.ts

--- trip-draw/vite.config.ts
+++ trip-draw/vite.config.ts
@@ -12,19 +12,19 @@ export default defineConfig({
     proxy: {
       // 정적 파일 (이미지 등)
       '/uploads': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         // 필요하다면 경로 재작성도 지정할 수 있습니다.
         // rewrite: (p) => p.replace(/^\/uploads/, '/uploads')
       },
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,           // HTTPS일 때 인증서 무시
         rewrite: (path) => path // 혹은 path.replace(/^\/api/, '')
       },
       '/oauth2': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
