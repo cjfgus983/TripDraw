a7d2a77ef82941418395c157e93dde7fdd3c0a8a
munncoding <cjfgus983@gmail.com>
2025-05-28 05:34:14 +0900

    Update FindIdPassword.vue

--- trip-draw/src/pages/user/FindIdPassword.vue
+++ trip-draw/src/pages/user/FindIdPassword.vue
@@ -157,7 +157,7 @@ import { ref } from "vue";
 import axios from "axios";
 
 // API 기본 주소 설정
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "/test";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "/api";
 
 // 활성 탭 상태
 const activeTab = ref<"findId"|"findPassword">("findId");
